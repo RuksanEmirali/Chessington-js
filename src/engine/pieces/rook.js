9c945913a1f727d2f29dfa286afad39861aa35cf
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-24 10:27:58 +0100

    Rook cannot move through other pieces, can take opponents but not the king

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Square from '../square';
 import GameSettings from '../gameSettings';
+import King from './king';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -16,17 +17,32 @@ export default class Rook extends Piece {
         return board.getPiece(Square.at(x, y)) !== undefined
     }
 
+    isOpponent(board, x, y){
+        return this.player != board.getPiece(Square.at(x,y)).player
+    }
+
+    isKing(board, x, y){
+        return (board.getPiece(Square.at(x, y)) instanceof King)
+    }
+
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let posMoves = []
         for (let i = location.col-1; i>= 0 ; i--) {
             if (this.isOccupied(board, location.row, i)){
+                if (this.isOpponent(board, location.row, i) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, location.row, i)
+                }
                 break
+            } else {
+                this.addMove(posMoves, location.row, i)
             }
-            this.addMove(posMoves, location.row, i)
         }
         for (let i = location.col + 1; i< GameSettings.BOARD_SIZE; i++) {
             if (this.isOccupied(board, location.row, i)){
+                if (this.isOpponent(board, location.row, i) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, location.row, i)
+                }
                 break
             } else {
                 this.addMove(posMoves, location.row, i)
@@ -34,6 +50,9 @@ export default class Rook extends Piece {
         }
         for (let i = location.row-1; i>=0 ; i--) {
             if (this.isOccupied(board, i, location.col)){
+                if (this.isOpponent(board, i, location.col) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, i, location.col)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, location.col)
@@ -41,6 +60,9 @@ export default class Rook extends Piece {
         }
         for (let i = location.row + 1; i< GameSettings.BOARD_SIZE; i++) {
             if (this.isOccupied(board, i, location.col)){
+                if (this.isOpponent(board, i, location.col) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, i, location.col)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, location.col)
