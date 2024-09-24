c1477f8c3cc533c4fe22bc936257e91679f75e6b
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-24 11:23:05 +0100

    King can move 1 space and must stays on board

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,39 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class King extends Piece {
     constructor(player) {
         super(player);
     }
 
+    addMove(posMoves, x, y){
+        posMoves.push(Square.at(x, y))
+    }
+
+    isOnBoard(board, x, y){
+        return (x < GameSettings.BOARD_SIZE 
+            && y < GameSettings.BOARD_SIZE
+            && x>= 0 && y >= 0)
+    }
+
     getAvailableMoves(board) {
-        return new Array(0);
+        let posMoves = []
+        let location = board.findPiece(this)
+        for (let i = location.col-1; i<location.col+2; i++){
+            if(this.isOnBoard(board, location.row + 1, i)) {
+                this.addMove(posMoves, location.row + 1, i)
+            }
+            if(this.isOnBoard(board, location.row - 1, i)) {
+                this.addMove(posMoves, location.row - 1, i)
+            }
+        }
+        if(this.isOnBoard(board, location.row, location.col-1)) {
+            this.addMove(posMoves, location.row, location.col-1)
+        }
+        if(this.isOnBoard(board, location.row, location.col+1)) {
+            this.addMove(posMoves, location.row, location.col+1)
+        }
+        return posMoves;
     }
 }
