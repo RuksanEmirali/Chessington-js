dd48b25107498c8500c9faf4e918a9fbd3dc1d7d
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-24 10:41:57 +0100

    Bishop can take opponents but not the King

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,7 +1,8 @@
 import Piece from './piece';
 import GameSettings from '../gameSettings';
 import Square from '../square';
-
+import King
+ from './king';
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
@@ -15,6 +16,14 @@ export default class Bishop extends Piece {
         return board.getPiece(Square.at(x, y)) !== undefined
     }
 
+    isOpponent(board, x, y){
+        return this.player != board.getPiece(Square.at(x,y)).player
+    }
+
+    isKing(board, x, y){
+        return (board.getPiece(Square.at(x, y)) instanceof King)
+    }
+
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let posMoves = []
@@ -22,6 +31,9 @@ export default class Bishop extends Piece {
         let j = location.col + 1
         while (i < GameSettings.BOARD_SIZE && j < GameSettings.BOARD_SIZE){
             if (this.isOccupied(board, i, j)){
+                if (this.isOpponent(board, i , j) && !this.isKing(board, i, j)){
+                    this.addMove(posMoves, i, j)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, j)
@@ -33,6 +45,9 @@ export default class Bishop extends Piece {
         j = location.col - 1
         while (i >= 0 && j >= 0){
             if (this.isOccupied(board, i, j)){
+                if (this.isOpponent(board, i , j) && !this.isKing(board, i, j)){
+                    this.addMove(posMoves, i, j)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, j)
@@ -44,6 +59,9 @@ export default class Bishop extends Piece {
         j = location.col + 1
         while (i >= 0 && j < GameSettings.BOARD_SIZE){
             if (this.isOccupied(board, i, j)){
+                if (this.isOpponent(board, i , j) && !this.isKing(board, i, j)){
+                    this.addMove(posMoves, i, j)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, j)
@@ -55,6 +73,9 @@ export default class Bishop extends Piece {
         j = location.col - 1
         while (i < GameSettings.BOARD_SIZE && j >=0){
             if (this.isOccupied(board, i, j)){
+                if (this.isOpponent(board, i , j) && !this.isKing(board, i, j)){
+                    this.addMove(posMoves, i, j)
+                }
                 break
             } else {
                 this.addMove(posMoves, i, j)
