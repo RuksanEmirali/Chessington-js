3110a184478aed348f30c382d1a0c36981d659f9
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-24 09:52:58 +0100

    Rook cannot move through other pieces

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -12,20 +12,39 @@ export default class Rook extends Piece {
         posMoves.push(Square.at(x, y))
     }
 
+    isOccupied(board, x, y){
+        return board.getPiece(Square.at(x, y)) !== undefined
+    }
+
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let posMoves = []
-        for (let i = 0; i< location.col; i++) {
+        for (let i = location.col-1; i>= 0 ; i--) {
+            if (this.isOccupied(board, location.row, i)){
+                break
+            }
             this.addMove(posMoves, location.row, i)
         }
         for (let i = location.col + 1; i< GameSettings.BOARD_SIZE; i++) {
-            this.addMove(posMoves, location.row, i)
+            if (this.isOccupied(board, location.row, i)){
+                break
+            } else {
+                this.addMove(posMoves, location.row, i)
+            }
         }
-        for (let i = 0; i< location.row; i++) {
-            this.addMove(posMoves, i, location.col)
+        for (let i = location.row-1; i>=0 ; i--) {
+            if (this.isOccupied(board, i, location.col)){
+                break
+            } else {
+                this.addMove(posMoves, i, location.col)
+            }
         }
         for (let i = location.row + 1; i< GameSettings.BOARD_SIZE; i++) {
-            this.addMove(posMoves, i, location.col)
+            if (this.isOccupied(board, i, location.col)){
+                break
+            } else {
+                this.addMove(posMoves, i, location.col)
+            }
         }
         return posMoves;
     }
