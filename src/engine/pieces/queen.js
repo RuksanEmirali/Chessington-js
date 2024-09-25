4dddcffff2ffa7ecf62e5483071e788e3c03f2c2
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-25 10:20:33 +0100

    Queen can move laterally

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,73 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
+import GameSettings from '../gameSettings';
+import King from './king';
 
 export default class Queen extends Piece {
     constructor(player) {
         super(player);
     }
+    
+    addMove(posMoves, x, y){
+        posMoves.push(Square.at(x, y))
+    }
+
+    isOccupied(board, x, y){
+        return board.getPiece(Square.at(x, y)) !== undefined
+    }
+
+    isOpponent(board, x, y){
+        return this.player != board.getPiece(Square.at(x,y)).player
+    }
+
+    isKing(board, x, y){
+        return (board.getPiece(Square.at(x, y)) instanceof King)
+    }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let posMoves = []
+        for (let i = location.col-1; i>= 0 ; i--) {
+            if (this.isOccupied(board, location.row, i)){
+                if (this.isOpponent(board, location.row, i) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, location.row, i)
+                }
+                break
+            } else {
+                this.addMove(posMoves, location.row, i)
+            }
+        }
+        for (let i = location.col + 1; i< GameSettings.BOARD_SIZE; i++) {
+            if (this.isOccupied(board, location.row, i)){
+                if (this.isOpponent(board, location.row, i) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, location.row, i)
+                }
+                break
+            } else {
+                this.addMove(posMoves, location.row, i)
+            }
+        }
+        for (let i = location.row-1; i>=0 ; i--) {
+            if (this.isOccupied(board, i, location.col)){
+                if (this.isOpponent(board, i, location.col) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, i, location.col)
+                }
+                break
+            } else {
+                this.addMove(posMoves, i, location.col)
+            }
+        }
+        for (let i = location.row + 1; i< GameSettings.BOARD_SIZE; i++) {
+            if (this.isOccupied(board, i, location.col)){
+                if (this.isOpponent(board, i, location.col) && !this.isKing(board, location.row, i)){
+                    this.addMove(posMoves, i, location.col)
+                }
+                break
+            } else {
+                this.addMove(posMoves, i, location.col)
+            }
+        }
+        return posMoves;
     }
 }
--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,30 @@
 import Queen from '../../../src/engine/pieces/queen';
+import Rook from '../../../src/engine/pieces/rook';
+import Pawn from '../../../src/engine/pieces/pawn';
+import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Queen', () => {
+    
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move laterally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(1, 2), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Horizontal
+            Square.at(1, 0), Square.at(1, 1), Square.at(1, 3), Square.at(1, 4), Square.at(1, 5), Square.at(1, 6), Square.at(1, 7),
+            // Vertical
+            Square.at(0, 2), Square.at(2, 2), Square.at(3, 2), Square.at(4, 2), Square.at(5, 2), Square.at(6, 2), Square.at(7, 2)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
 
 });
