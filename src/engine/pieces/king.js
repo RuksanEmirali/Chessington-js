6b371be87293a4341fb902064d1583e0f9d0171a
ruksan_emirali <ruksanemirali@gmail.com>
2024-09-25 10:02:00 +0100

    King cannont take friendly pieces, can take opponents but not the king

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -17,21 +17,45 @@ export default class King extends Piece {
             && x>= 0 && y >= 0)
     }
 
+    isEmpty(board, x, y){
+        return board.getPiece(Square.at(x, y)) === undefined
+    }
+
+    isOpponent(board, x, y){
+        return this.player != board.getPiece(Square.at(x,y)).player
+    }
+
+    isKing(board, x, y){
+        return (board.getPiece(Square.at(x, y)) instanceof King)
+    }
+
     getAvailableMoves(board) {
         let posMoves = []
         let location = board.findPiece(this)
         for (let i = location.col-1; i<location.col+2; i++){
-            if(this.isOnBoard(board, location.row + 1, i)) {
+            if(this.isOnBoard(board, location.row + 1, i)
+                && (this.isEmpty(board, location.row + 1, i)
+                    ||(this.isOpponent(board, location.row + 1, i)
+                        && !this.isKing(board, location.row + 1, i)))) {
                 this.addMove(posMoves, location.row + 1, i)
             }
-            if(this.isOnBoard(board, location.row - 1, i)) {
+            if(this.isOnBoard(board, location.row - 1, i)
+                && (this.isEmpty(board, location.row - 1, i)
+                    ||(this.isOpponent(board, location.row - 1, i)
+                        && !this.isKing(board, location.row - 1, i)))) {
                 this.addMove(posMoves, location.row - 1, i)
             }
         }
-        if(this.isOnBoard(board, location.row, location.col-1)) {
+        if(this.isOnBoard(board, location.row, location.col-1)
+            && (this.isEmpty(board, location.row, location.col-1)
+                ||(this.isOpponent(board, location.row, location.col-1)
+                    && !this.isKing(board, location.row, location.col-1)))) {
             this.addMove(posMoves, location.row, location.col-1)
         }
-        if(this.isOnBoard(board, location.row, location.col+1)) {
+        if(this.isOnBoard(board, location.row, location.col+1)
+            && (this.isEmpty(board, location.row, location.col+1)
+                ||(this.isOpponent(board, location.row, location.col+1)
+                    && !this.isKing(board, location.row, location.col+1)))) {
             this.addMove(posMoves, location.row, location.col+1)
         }
         return posMoves;
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -2,6 +2,7 @@ import King from '../../../src/engine/pieces/king';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
+import Rook from '../../../src/engine/pieces/rook';
 
 describe('King', () => {
 
@@ -40,5 +41,38 @@ describe('King', () => {
         const expectedMoves = [Square.at(0, 1), Square.at(1, 1), Square.at(1, 0)];
 
         moves.should.deep.have.members(expectedMoves);
+    });
+
+    it('cannot take a friendly piece', () => {
+        const king = new King(Player.BLACK);
+        const friendlyPiece = new Rook(Player.BLACK);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(3, 3), friendlyPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(3, 3));
+    });
+
+    it('can take an opponents piece', () => {
+        const king = new King(Player.BLACK);
+        const opponentPiece = new Rook(Player.WHITE);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(3, 3), opponentPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(3, 3));
+    });
+
+    it('cannot take the opposing king', () => {
+        const king = new King(Player.BLACK);
+        const opposingKing = new King(Player.WHITE);
+        board.setPiece(Square.at(4, 4), king);
+        board.setPiece(Square.at(3, 3), opposingKing);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(3, 3));
     });
 });
